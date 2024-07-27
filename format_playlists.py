340fb9d9c24ea7c0a04c9c471b419083d1722a60
Minimac21 <mac.holland.d@gmail.com>
2024-07-27 16:11:09 -0500

    paths for laptop, and downloading music as mp3s

--- format_playlists.py
+++ format_playlists.py
@@ -3,7 +3,7 @@
 import json
 
 lines = []
-with open("/home/mac/youtube-backup/LEDGER/playlists.txt", "r") as f:
+with open("/home/mac/projects/youtube-backup/LEDGER/playlists.txt", "r") as f:
     lines = f.readlines()
 
 playlists=[]
@@ -11,11 +11,12 @@ playlist_names=[]
 
 for line in lines:
     line=json.loads(line)
-    playlists.append(line["url"])
-    playlist_names.append(line["title"])
+    if line["title"] != "musica":
+        playlists.append(line["url"])
+        playlist_names.append(line["title"])
 
 
-with open("/home/mac/youtube-backup/LEDGER/playlists.txt", "w") as f:
+with open("/home/mac/projects/youtube-backup/LEDGER/playlists.txt", "w") as f:
     for i, playlist in enumerate(playlists):
         name = playlist_names[i]
         f.write(playlist + " " + name + "\n")
