b530ac388a6fbccf106e4785ca1558e99663294e
Minimac21 <mac.holland.d@gmail.com>
2024-08-02 01:09:51 -0500

    added bap clips but didn't yet DL

--- format_playlists.py
+++ format_playlists.py
@@ -6,11 +6,11 @@ lines = []
 with open("/home/mac/projects/youtube-backup/LEDGER/playlists.txt", "r") as f:
     lines = f.readlines()
 
-playlists=[]
-playlist_names=[]
+playlists = []
+playlist_names = []
 
 for line in lines:
-    line=json.loads(line)
+    line = json.loads(line)
     if line["title"] != "musica":
         playlists.append(line["url"])
         playlist_names.append(line["title"])
